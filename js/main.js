e6cf5c0ff2f1300dcd2194ec209f278077eaa6e0
Rulerofdragons3 <147750572+Rulerofdragons3@users.noreply.github.com>
2026-02-25 21:35:31 -0600

    Added download page for FNG and tweaked logos

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ console.log("Main is alive")
 const navbar = document.getElementById("navbar")
 navbar.innerHTML = `
 <a id="logo-lin" class="navbar-link" href="/">
-    <img src="/assets/Logo.png" height="50" class="borderless" style="margin-bottom: -10%;"></img>
+    <img src="/assets/Corner Logo.gif" height="50" class="borderless" style="margin-bottom: -10%;"></img>
 </a>
 <a class="navbar-link" href="/downloads/">Downloads</a>
 <a class="navbar-link" href="/about-me/">About Me</a>
